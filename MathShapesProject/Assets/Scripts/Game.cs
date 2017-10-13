22ee950989c56c890836a4feeeecadafec110627
Garipov Ilyas <garillas.feedback@gmail.com>
2017-10-14 00:17:13 +0300

    Revert "github test changes_1"
    This reverts commit 184f09d7c984da421c7cbd2ac0cb1c4c408a37bd.

--- MathShapesProject/Assets/Scripts/Game.cs
+++ MathShapesProject/Assets/Scripts/Game.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+<<<<<<< HEAD
 
     private int Free_Gold;
+=======
+>>>>>>> parent of 184f09d... github test changes_1
 	// Use this for initialization
 	void Start () {
 		
