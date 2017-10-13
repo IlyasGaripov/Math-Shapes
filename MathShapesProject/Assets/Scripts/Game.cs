f080e5ab1e6ed98497dbb5f9f1725a92acba784f
Garipov Ilyas <garillas.feedback@gmail.com>
2017-10-14 00:32:06 +0300

    added test cycle

--- MathShapesProject/Assets/Scripts/Game.cs
+++ MathShapesProject/Assets/Scripts/Game.cs
@@ -8,7 +8,10 @@ public class Game : MonoBehaviour {
     private int coins;
 	// Use this for initialization
 	void Start () {
-		
+		for(int i = 0; i <10; i++)
+        {
+            coins++;
+        }
 	}
 	
 	// Update is called once per frame
