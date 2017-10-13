0bd939567fca7abfe5ad0b595f2e8df5ed7ae8b2
Garipov Ilyas <garillas.feedback@gmail.com>
2017-10-14 00:18:49 +0300

    try git work
    This reverts commit 184f09d7c984da421c7cbd2ac0cb1c4c408a37bd.

--- MathShapesProject/Assets/Scripts/Game.cs
+++ MathShapesProject/Assets/Scripts/Game.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
-<<<<<<< HEAD
-
     private int Free_Gold;
-=======
->>>>>>> parent of 184f09d... github test changes_1
+
 	// Use this for initialization
 	void Start () {
 		
